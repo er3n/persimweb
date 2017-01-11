69c54ab1182771f4f265820719e1d02dd1dfedce
er3n <erenozturk@gmail.com>
2017-01-11 23:18:36 +0200

    paket adi düzenlendi

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -23,12 +23,12 @@ class Navbar extends Component {
         return (
             
             <nav className="navbar navbar-toggleable-md navbar-inverse bg-inverse fixed-top">
-                <button onClick={this.handleToggle} className="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
+                <button onClick={this.handleToggle} className="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon" />
                 </button>
                 <a className="navbar-brand" href="#" >{title}</a>
 
-                <div className={classNames('collapse', 'navbar-collapse', {show: this.state.isToggleOn})} id="navbarsExampleDefault">
+                <div className={classNames('collapse', 'navbar-collapse', {show: this.state.isToggleOn})} >
                     <ul className="navbar-nav mr-auto">
                         <li className="nav-item active">
                             <a className="nav-link" href="#">Ders Programı <span className="sr-only">(current)</span></a>
